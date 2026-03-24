c49e23e843ccbc73b028c9bbf2a5de16dc321f88
Seoyeon Park <68544849+seoyeon3@users.noreply.github.com>
2026-03-24 20:25:56 +1100

    Update helpers.sql

--- Assignment2/helpers.sql
+++ Assignment2/helpers.sql
@@ -1,8 +1,3 @@
--- COMP3311 21T3 Ass2 ... extra database definitions
--- add any views or functions you need into this file
--- note: it must load without error into a freshly created mymyunsw database
--- you must submit this even if you add nothing to it
-
 -- Q1
 create or replace view q1(student,CourseCode, Term, CourseTitle, Mark, Grade, UOC, family, given) 
 as
